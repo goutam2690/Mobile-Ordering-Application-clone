06e5b2124df5d4581d6f3aa162ced51c73d07027
goutam2690 <124591682+goutam2690@users.noreply.github.com>
2023-12-24 14:56:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+    assetsDir: 'assets', // Specify the assets directory for the production build
+    sourcemap: true, // Enable source maps for production build
+  },
+  server: {
+    port: 3000, // Specify the port for the development server
+    proxy: {
+      '/api': 'http://localhost:5045', // Example: Proxy API requests to a local server
+    },
+  },
 })
+
